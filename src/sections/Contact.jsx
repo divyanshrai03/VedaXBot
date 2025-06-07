1737f3d2a3dd17b1f3a127601540fa7c674b5550
Divyansh Rai <ce.divyanshrai@gmail.com>
2025-06-07 13:48:43 +0530

    Contact

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -2,7 +2,8 @@ import { useRef, useState } from "react";
 import emailjs from "@emailjs/browser";
 
 import TitleHeader from "../components/TitleHeader";
-import ContactExperience from "../components/models/contact/ContactExperience";
+import ContactExperience from "../components/Models/contact/ContactExperience";
+// import ContactExperience from "../components/models/contact/ContactExperience";
 
 const Contact = () => {
   const formRef = useRef(null);
@@ -109,7 +110,7 @@ const Contact = () => {
           </div>
           <div className="xl:col-span-7 min-h-96">
             <div className="bg-[#cd7c2e] w-full h-full hover:cursor-grab rounded-3xl overflow-hidden">
-              <ContactExperience />
+              <ContactExperience/>
             </div>
           </div>
         </div>
